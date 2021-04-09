9c4312f8fc222fc3b69aa796138f6f8ba3d84708
Arek Chatmajian <arek.chatmajian@gmail.com>
2021-04-09 15:17:33 -0400

    added calculations for balance, income, and expense

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const amount = document.getElementById('amount');
 const dummyTransactions = [
     { id: 1, text: 'Flower', amount: -20 },
     { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -20 },
+    { id: 3, text: 'Book', amount: -10 },
     { id: 4, text: 'Camera', amount: 150 },
 ];
 
@@ -33,11 +33,31 @@ function addTransactionDOM(transaction) {
     list.appendChild(item);
 }
 
+// Update the balance, income, and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+    const expense = (amounts
+    .filter(item => item < 0)
+    .reduce((acc, item) => (acc += item), 0) * -1)
+    .toFixed(2);
+
+    console.log(expense);
+}
+
 // Init app
 function init() {
     list.innerHTML = '';
 
     transactions.forEach(addTransactionDOM);
+    updateValues();
 }
 
 init();
