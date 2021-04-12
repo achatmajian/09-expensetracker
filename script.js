8534eeb7340333af0e6ab733ced5a5a9c2a6c5de
Arek Chatmajian <arek.chatmajian@gmail.com>
2021-04-12 12:49:26 -0400

    can now remove transactions

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function addTransactionDOM(transaction) {
 
     item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
-    <button class="delete-btn">x</button>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
     `;
 
     list.appendChild(item);
@@ -81,6 +81,13 @@ function updateValues() {
     balance.innerText = `$${total}`;
     money_plus.innerText = `$${income}`;
     money_minus.innerText = `$${expense}`;
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+
+    init();
 }
 
 // Init app
